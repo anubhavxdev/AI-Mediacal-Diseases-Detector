acbf4aa6a29c4a60abbb18c3605a39858b2adc05
Anubhav Jaiswal <anubhavjiaswal1803@gmail.com>
2025-01-22 23:56:14 +0530

    basic AI model setup

---
+++ processor.py
@@ -0,0 +1,47 @@
+import os
+import base64
+from groq import Groq
+
+api_key = os.environ.get('GROQ_API_KEY')
+
+if not api_key:
+    raise ValueError("API key is not set. Please set the GROQ_API_KEY environment variable.")
+
+image_path = 'acne.png'
+with open(image_path, 'rb') as image_file:
+    encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+client = Groq(api_key=api_key)
+
+query = 'what happened on my face?'
+model = "llama-3.2-90b-vision-preview"
+
+message = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text": query
+            }
+        ]
+    },
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{encoded_image}"
+                }
+            }
+        ]
+    }
+]
+
+chat_completion = client.chat.completions.create(
+    model=model,
+    messages=message,
+)
+
+print(chat_completion.choices[0].message.content)
